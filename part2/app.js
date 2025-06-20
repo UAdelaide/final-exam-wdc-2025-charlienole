cc0c5c4e9cd3e0535764f09b3c9b775ba092c19b
Charlie <a1849667@adelaide.edu.au>
2025-06-20 04:55:12 +0000

    [automatic commit] 2025-06-20 04:55:11 UTC 129.127.204.92

--- part2/app.js
+++ part2/app.js
@@ -27,6 +27,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes); // added
 
 // Export the app instead of listening here
 module.exports = app;
