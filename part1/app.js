6f5f2468c147b2b9f92f6f4aee40757d04a54884
Charlie <a1849667@adelaide.edu.au>
2025-06-20 03:35:34 +0000

    [automatic commit] 2025-06-20 03:35:33 UTC 129.127.204.92

--- part1/app.js
+++ part1/app.js
@@ -156,17 +156,17 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
     const [result] = await db.execute(`
-      SELECT
-        WalkRequests.request_id,
-        Dogs.name AS dog_name,
-        WalkRequests.requested_time,
-        WalkRequests.duration_minutes,
-        WalkRequests.location,
-        Users.username AS owner_username
-      FROM WalkRequests
-      JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-      JOIN Users ON Dogs.owner_id = Users.user_id
-      WHERE WalkRequests.status = 'open';
+        SELECT
+            WalkRequests.request_id,
+            Dogs.name AS dog_name,
+            WalkRequests.requested_time,
+            WalkRequests.duration_minutes,
+            WalkRequests.location,
+            Users.username AS owner_username
+        FROM WalkRequests
+        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+        JOIN Users ON Dogs.owner_id = Users.user_id
+        WHERE WalkRequests.status = 'open';
     `);
     res.json(result);
   } catch (err) {
