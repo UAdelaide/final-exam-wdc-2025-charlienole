2508fd541dbda75ac2fe6e01218566c22bb67a32
Charlie <a1849667@adelaide.edu.au>
2025-06-20 03:35:56 +0000

    [automatic commit] 2025-06-20 03:35:55 UTC 129.127.204.92

--- part1/app.js
+++ part1/app.js
@@ -41,13 +41,13 @@ let db;
 
     // create tables as per provided dogwalks.sql file
     await db.query(`
-      CREATE TABLE Users (
-          user_id INT AUTO_INCREMENT PRIMARY KEY,
-          username VARCHAR(50) UNIQUE NOT NULL,
-          email VARCHAR(100) UNIQUE NOT NULL,
-          password_hash VARCHAR(255) NOT NULL,
-          role ENUM('owner', 'walker') NOT NULL,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        CREATE TABLE Users (
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            role ENUM('owner', 'walker') NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
 
       CREATE TABLE Dogs (
@@ -139,12 +139,12 @@ let db;
 app.get('/api/dogs', async (req, res) => {
     try {
     const [result] = await db.execute(`
-      SELECT
-        Dogs.name AS dog_name,
-        Dogs.size,
-        Users.username AS owner_username
-      FROM Dogs
-      JOIN Users ON Dogs.owner_id = Users.user_id;
+        SELECT
+            Dogs.name AS dog_name,
+            Dogs.size,
+            Users.username AS owner_username
+        FROM Dogs
+        JOIN Users ON Dogs.owner_id = Users.user_id;
     `);
     res.json(result);
   } catch (err) {
