b6755a8b6d4a0fb8f4c012fc2bb08d0ca65c1a1d
Charlie <a1849667@adelaide.edu.au>
2025-06-20 03:23:12 +0000

    [automatic commit] 2025-06-20 03:23:11 UTC 129.127.204.92

--- part1/app.js
+++ part1/app.js
@@ -14,6 +14,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// database code added below
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
