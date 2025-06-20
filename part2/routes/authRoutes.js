57aaaa173978be71b1bc58996177ef001ae9d3ad
Charlie <a1849667@adelaide.edu.au>
2025-06-20 04:53:47 +0000

    [automatic commit] 2025-06-20 04:53:46 UTC 129.127.204.92

--- part2/routes/authRoutes.js
+++ part2/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// POST /api/auth/login
+// POST /auth/login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
