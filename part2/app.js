f04c816b572d3b51de8e5bedabfa2dbde8e9f184
Charlie <a1849667@adelaide.edu.au>
2025-06-20 04:37:13 +0000

    [automatic commit] 2025-06-20 04:37:13 UTC 129.127.204.92

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+const session = require('express-session'); // added
+
 const app = express();
 
 // Middleware
